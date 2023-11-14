a1a46a1ff2652ceb9f454f92dd84054da557ffcf
hanome1 <divanchmo@gmail.com>
2023-11-14 17:46:53 +0300

    sem1

---
+++ sem1.py
@@ -0,0 +1,11 @@
+import subprocess
+
+if __name__ == '__main__':
+    res = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = res.stdout
+    if res.returncode == 0:
+        lst = out.split('\n')
+        if 'VERSION="22.04.3 LTS (Jammy Jellyfish)"' in lst and 'VERSION_CODENAME=jammy' in lst:
+            print('success')
+    else:
+        print('fail')
