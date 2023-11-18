46c8df5450dd4bafe20540eec8334449656f59ae
hanome1 <divanchmo@gmail.com>
2023-11-18 18:47:39 +0300

    hw2 task1 done

--- test_1_pos.py
+++ test_1_pos.py
@@ -1,13 +1,15 @@
 import subprocess
 
-tst = "/home/user/tst"
-out = "/home/user/out"
-folder1 = "/home/user/folder1"
+main = "/home/lemonah/tests"
+tst = "/home/lemonah/tests/tst"
+out = "/home/lemonah/tests/out"
+folder1 = "/home/lemonah/tests/folder1"
 
 
 def checkout(cmd, text):
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     if text in result.stdout and result.returncode == 0:
+        # print(result.stdout)
         return True
     else:
         return False
@@ -48,7 +50,7 @@ def test_step1():
 
 
 def test_step2():
-    # test1 ======== take docs from folder: out and copy this docs to folder1
+    # test1 ======== take docs from folder: out and copy these docs to folder1
     result1 = checkout("cd /home/lemonah/tests/out; 7z e arx2.7z -o/home/lemonah/tests/folder1 -y", "Everything is Ok")
     result2 = checkout("cd /home/lemonah/tests/folder1; ls", "qwe")
     result3 = checkout("cd /home/lemonah/tests/folder1; ls", "rty")
@@ -56,32 +58,25 @@ def test_step2():
     # assert checkout("cd /home/lemonah/tests/folder1; ls", ["qwe", "rty"]) # ne robit
 
 
-# def test_step1():
-#     # test1 =================== create archive
-#     result1 = checkout("cd {}; 7z a {}/arx2".format(tst, out), "Everything is Ok")
-#     # check if arx2.7z in out
-#     result2 = checkout("cd {}; ls".format(out), "arx2.7z")
-#     assert result1 and result2, "Test1 FAIL"
-#
-#
-# def test_step2():
-#     # test1 ======== take docs from folder: out and copy this docs to folder1
-#     result1 = checkout("cd {}; 7z e arx2.7z -o{} -y".format(out, folder1), "Everything is Ok")
-#     result2 = checkout("cd {}; ls".format(folder1), "one")
-#     result3 = checkout("cd {}; ls".format(folder1), "two")
-#     assert result1 and result2 and result3, "Test2 FAIL"
-#
-#
-# def test_step3():
-#     # test3 =========show info about arx2.7z
-#     assert checkout("cd {}; 7z t arx2.7z".format(out), "Everything is Ok"), "Test3 FAIL"
-#
-#
-# def test_step4():
-#     # test4 ========= add archive update
-#     assert checkout("cd {}; 7z u {}/arx2.7z".format(tst, out), "Everything is Ok"), "Test4 FAIL"
-#
-#
-# def test_step5():
-#     # test5 ========= delete docs one and two from archive in folder out
-#     assert checkout("cd {}; 7z d arx2.7z".format(out), "Everything is Ok"), "Test5 FAIL"
+def test_step6():
+    """TESTING FLAG l"""
+
+    stock_files = subprocess.run(f'ls {tst}', shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout.split()
+    # print('\n', stock_files)
+    for item in stock_files:
+        assert checkout(f'cd {out}; 7z l arx2.7z', item)
+
+
+def test_step7():
+    """TESTING FLAG x"""
+
+    subprocess.run(f'cd {main}; 7z a {out}/arc_subfol', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    stock_files = subprocess.run(f'ls {main}', shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout.split()
+    # print('\n\nSTOCK\n', stock_files)
+    # print(subprocess.run(f'cd {out}; 7z l arc_subfol.7z').stdout)
+    for item in stock_files:
+        assert checkout(f'cd {out}; 7z l arc_subfol.7z', item)
+
+
+def test_step8():
+    pass
